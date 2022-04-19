0e63124ee597a87434acdbc0731fdde263a6f144
yaseenawazah <yaseen.awazah@jayway.com>
2022-04-19 16:09:18 +0200

    Change port so that it works with cloud run

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ const middlewares = jsonServer.defaults({
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+server.listen(8080, () => {
   console.log("JSON Server is running");
 });
