36e1f7d0eca5597ab88e5318d020c6b6322759ed
yaseenawazah <yaseen.awazah@jayway.com>
2022-04-21 11:29:07 +0200

    Add no cors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const middlewares = jsonServer.defaults({
 });
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 server.use(router);
 server.listen(8080, () => {
   console.log("JSON Server is running");
